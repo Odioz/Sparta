d909beea68f25aeaa40d448ad06280220d2853c4
Odioz <odiozsoft@gmail.com>
2021-04-12 22:29:53 +0900

    sparta1

---
+++ lib/tab3.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Tab3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TabMain();
+  }
+}
+
+/// This is the stateless widget that the main application instantiates.
+class TabMain extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: Text('Это третья вкладка'),
+    ));
+  }
+}
