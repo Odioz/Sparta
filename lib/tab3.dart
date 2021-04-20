07bf02c2dca5955378404736e6398dbb84c8289b
Odioz <odiozsoft@gmail.com>
2021-04-20 12:19:28 +0900

    prokol_imag

--- lib/tab3.dart
+++ lib/tab3.dart
@@ -1,4 +1,6 @@
 // вкладка для показа протоколов
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:sparta/domain/protocol.dart';
 
@@ -34,7 +36,7 @@ class ProtocolList extends StatelessWidget {
         protocol: 'Подъем по трехколенной лестнице',
         url: 'https://testrtp.ru/img/protokol004.jpg'),
     Protocol(
-        protocol: 'Закрепление спас.веревки за конструкцию',
+        protocol: 'Закрепление спасательной веревки за конструкцию',
         url: 'https://testrtp.ru/img/protokol003.jpg'),
     Protocol(
         protocol: 'Эстафета', url: 'https://testrtp.ru/img/protokol004.jpg'),
@@ -44,14 +46,51 @@ class ProtocolList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        //padding: EdgeInsets.all(8),
+        padding: EdgeInsets.all(8),
         child: GridView.builder(
-      itemCount: protocols.length,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
-      itemBuilder: (context, int i) {
-        return Image.network(protocols[i].url);
-      },
-    ));
+          itemCount: protocols.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, crossAxisSpacing: 4.0, mainAxisSpacing: 4.0),
+          itemBuilder: (context, int i) {
+            return GestureDetector(
+              child: Card(
+                color: Color.fromRGBO(24, 26, 27, 1),
+                margin: EdgeInsets.all(10),
+                child: getCard(i),
+              ),
+              onTap: () {
+                //
+              },
+            );
+          },
+        ));
+  }
+
+  Column getCard(int i) {
+    String messg = protocols[i].protocol;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new Center(
+          child: Container(
+            child: new Stack(
+              children: <Widget>[
+                new Image.asset(
+                  'images/protokol.jpg',
+                  width: 200,
+                  height: 160,
+                )
+              ],
+            ),
+          ),
+        ),
+        Text(
+          messg,
+          style:
+              TextStyle(color: Color.fromRGBO(254, 182, 88, 1), fontSize: 16),
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
   }
 }
